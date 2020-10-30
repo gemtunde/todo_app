ba1d87dc96cd867e652ecddacd0f4a883158fe78
gemtunde <52404803+gemtunde@users.noreply.github.com>
2020-10-30 10:50:29 +0100

    Add files via upload

---
+++ build/precache-manifest.e10cb963569fad8daf6ced410968d41a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c77b35961ef3a15509788bac21dc3745",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0591c7a0fe387e5e5051",
+    "url": "/static/css/main.ec726d4e.chunk.css"
+  },
+  {
+    "revision": "26ad85cc5c37483e0b67",
+    "url": "/static/js/2.e5b5bde8.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/static/js/2.e5b5bde8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0591c7a0fe387e5e5051",
+    "url": "/static/js/main.364b9c97.chunk.js"
+  },
+  {
+    "revision": "9c6fb2a44e38d7c61afa",
+    "url": "/static/js/runtime-main.3f300b2a.js"
+  }
+]);
